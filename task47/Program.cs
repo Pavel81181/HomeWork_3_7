8bf4addb2d7a9c2e380e999636560f8a6adcf74c
Pavel Bazhin <p81181@yandex.ru>
2023-04-13 22:47:56 +0300

    Домашнее задание 7-й семинар

---
+++ task47/Program.cs
@@ -0,0 +1,52 @@
+﻿/* Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9 */
+
+Start();
+void Start()
+{
+    Console.Clear();
+    int rows = SetNumber("m");
+    int columns = SetNumber("n");
+    Console.WriteLine();
+    int[,] matrix = GetMatrix(rows, columns);
+    PrintMatrix(matrix);
+    Console.WriteLine();
+}
+
+int SetNumber(string text = "")
+{
+    Console.Write($"Enter number {text}: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+    return num;
+}
+double[,] GetMatrix(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+    
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round((randon.Next(minValue, maxValue ) + randon.NextDouble()),2);
+        }
+    }
+    return array;
+}
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLongLength(1); j++)
+        {
+            Console.Write($" {matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
